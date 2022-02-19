76dfa72190a86d9ef214016ef90c0da3aaec2174
Lylio <cxz@gmx.com>
2022-02-19 12:56:24 +0000

    css theme changed

---
+++ src/test/java/com/lylechristine/quarcode/config/ApplicationPropertiesTest.java
@@ -0,0 +1,29 @@
+package com.lylechristine.quarcode.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class ApplicationPropertiesTest {
+
+    @Autowired
+    ApplicationProperties applicationProperties;
+
+    @Test
+    public void thatTitleIsValid() {
+        String actual = this.applicationProperties.getTitle();
+        assertNotNull(actual);
+        assertEquals("quarcode", actual);
+    }
+
+    @Test
+    public void thatAppInfoIsValid() {
+        String actual = this.applicationProperties.getAppInfo();
+        assertNotNull(actual);
+        assertEquals("quarcode, 0.0.1-SNAPSHOT", actual);
+    }
+}
